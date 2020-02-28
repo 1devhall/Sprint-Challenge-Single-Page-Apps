1929036c45020a3cfcc2c6023b6afff1753fac49
1devhall <devoughnh@gmail.com>
2020-02-28 11:47:23 -0500

    finished mvp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+export default function CharacterCard(props) {
+  return (
+    <div>
+  <Card body className="text-center">
+    <CardTitle>{props.name}</CardTitle>
+    <CardText>{props.species}</CardText>
+  </Card>
+    </div>
+  );
+};
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { Jumbotron, Container } from 'reactstrap';
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Welcome to the ultimate fan site!</h1>
+          <img
+  className="main-img"
+  src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  alt="rick"
+/>
+        </Container>
+      </Jumbotron>
       </header>
     </section>
   );
